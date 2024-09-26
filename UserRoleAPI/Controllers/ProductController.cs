338161a70a0f8cdad7033369687c553a92b424d7
EmirhanSelcuk <88584792+EmirhanSelcuk@users.noreply.github.com>
2024-09-26 11:01:11 +0300

    Add project files.

---
+++ UserRoleAPI/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserRoleAPI.Data;
+using UserRoleAPI.Models;
+
+namespace UserRoleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "AdminPolicy")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+         return await _context.Products.ToListAsync();
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "AdminPolicy")]
+        public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+            // Kullanıcının admin olup olmadığını kontrol et
+            if (!User.IsInRole("AdminPolicy"))
+            {
+                return BadRequest(new { message = "Yalnızca admin kullanıcılar ürün ekleyebilir." });
+            }
+
+            // Modelin geçerliliğini kontrol et
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Products.ToListAsync());
+        }
+    }
+}
---
+++ UserRoleAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using UserRoleAPI.Models;
+
+namespace UserRoleAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options) {    }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
